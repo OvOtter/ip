a6ac224e04f1d3cac9ee97b49a5e3a66c9f35061
OVOtter <shenzihan319@gmail.com>
2025-01-24 00:48:03 +0800

    Complete Level-4

--- src/main/java/Bun.java
+++ src/main/java/Bun.java
@@ -1,7 +1,4 @@
-import java.util.Random;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Bun {
@@ -17,42 +14,11 @@ public class Bun {
                  | '--'  /('  '-'(_.-' |  | \\   |  \s
                  `------'   `-----'    `--'  `--'  \s
         """;
-    private final static String[] kaomoji = {
-            "(´･ω･`)",         // Basic happy face
-            "(･ω･｡)",           // Smiling face with blush
-            "(･∀･)",           // Happy face
-            "(`･ω･´)",         // Determined face
-            "(￣ー￣)",         // Smirking face
-            "(Ｔ▽Ｔ)",         // Crying face
-            "(´・ω・`)",       // Sad face
-            "(ง'̀-'́)ง",         // Fighting pose
-            "┬─┬ ノ( ゜-゜ノ)", // Put the table back
-            "ヽ(｀Д´)ﾉ",         // Angry
-            "(¬_¬)",           // Suspicious
-            "(｡♥‿♥｡)",         // Love
-            "(ﾟｰﾟ)",           // Neutral
-            "（￣へ￣）",         // Annoyed
-            "(*^_^*)",         // Shy smile
-            "(>_<)",           // Frustrated
-            "(*>ω<)",           // Excited
-            "（＾ｖ＾）",         // Cheerful
-            "(=^･ω･^=)",         // Cat face
-            "(⌒ω⌒)",           // Happy face
-            "(｀･ω･´)",         // Another determined face
-            "(≧◡≦)",           // Very happy
-            "(¬‿¬)",           // Wink
-            "(*¬∀¬)",           // Playful
-            "(✿◠‿◠)",           // Flower smile
-            "(°ロ°)☝",           // Surprise
-            "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧",     // Excited
-            "(◕‿◕)",           // Friendly
-            "(づ｡◕‿‿◕｡)づ"       // Hug
-    };
 
-    private final ArrayList<Task> toDoList;
+    private final ArrayList<Task> taskList;
 
     public Bun() {
-        this.toDoList = new ArrayList<>();
+        this.taskList = new ArrayList<>();
     }
 
     private static void intro() {
@@ -61,39 +27,73 @@ public class Bun {
                 "    What can I do for you?");
     }
 
-    public static void main(String[] args)
-            throws IOException{
+    private static void printAddTaskMessage(Task task, int taskCount) {
+        System.out.println("     Got it. I've added this task:\n" +
+                "       " + task + "\n" +
+                "     Now you have " + taskCount + " tasks in the list.");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Bun bun = new Bun();
 
         intro();
 
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        label:
         while (true) {
-            String s = r.readLine();
-            if (s.equals("bye")) {
-                System.out.println("    Bye. Hope to see you again soon!");
-                break;
-            } else if (s.equals("list")) {
-                System.out.println("    (ง'̀-'́)ง Here are the tasks in your list:");
-                for (int i = 0; i < bun.toDoList.size(); i++) {
-                    System.out.println("    " + (i + 1) + ". " + bun.toDoList.get(i));
+            String instruction = scanner.next();
+            switch (instruction) {
+                case "bye":
+                    System.out.println("    Bye. Hope to see you again soon!");
+                    break label;
+                case "list":
+                    System.out.println("    (ง'̀-'́)ง Here are the tasks in your list:");
+                    for (int i = 0; i < bun.taskList.size(); i++) {
+                        System.out.println("    " + (i + 1) + ". " + bun.taskList.get(i));
+                    }
+                    scanner.nextLine();
+                    break;
+                case "mark": {
+                    int index = Integer.parseInt(scanner.nextLine().trim()) - 1;
+                    Task curTask = bun.taskList.get(index);
+                    curTask.markAsDone();
+                    System.out.println("    Nice (*>ω<) I've marked this task as done:\n      " + curTask);
+                    break;
                 }
-            } else if (s.startsWith("mark ")) {
-                int index = Integer.parseInt(s.split("\\s+")[1]) - 1;
-                Task curTask = bun.toDoList.get(index);
-                curTask.markAsDone();
-                System.out.println("    Nice (*>ω<) I've marked this task as done:\n      " + curTask);
-            } else if (s.startsWith("unmark ")) {
-                int index = Integer.parseInt(s.split("\\s+")[1]) - 1;
-                Task curTask = bun.toDoList.get(index);
-                curTask.markAsNotDone();
-                System.out.println("    OK (`･ω･´) I've marked this task as not done yet:\n      " + curTask);
-            } else{
-                //randomly add one kaomoji at the end of the echo!
-                String randomKaomoji = kaomoji[new Random().nextInt(kaomoji.length)];
-                s += " " + randomKaomoji;
-                bun.toDoList.add(new Task(s));
-                System.out.println("    added: " + s);
+                case "unmark": {
+                    int index = Integer.parseInt(scanner.nextLine().trim()) - 1;
+                    Task curTask = bun.taskList.get(index);
+                    curTask.markAsNotDone();
+                    System.out.println("    OK (`･ω･´) I've marked this task as not done yet:\n      " + curTask);
+                    break;
+                }
+                case "todo": {
+                    String description = scanner.nextLine().trim();
+                    ToDo toDo = new ToDo(description);
+                    bun.taskList.add(toDo);
+                    printAddTaskMessage(toDo, bun.taskList.size());
+                    break;
+                }
+                case "deadline": {
+                    String[] content = scanner.nextLine().trim().split(" /by ");
+                    String description = content[0].trim();
+                    String date = content[1].trim();
+                    Deadline deadline = new Deadline(description, date);
+                    bun.taskList.add(deadline);
+                    printAddTaskMessage(deadline, bun.taskList.size());
+                    break;
+                }
+                case "event": {
+                    String[] content = scanner.nextLine().trim().split(" /from | /to ");
+                    String description = content[0].trim();
+                    String start = content[1].trim();
+                    String end = content[2].trim();
+                    Event event = new Event(description, start, end);
+                    bun.taskList.add(event);
+                    printAddTaskMessage(event, bun.taskList.size());
+                    break;
+                }
+
             }
         }
     }
