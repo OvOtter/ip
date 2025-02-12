74e8d84fe9a01b87de6d168ae2e9eebe16c3c412
OVOtter <shenzihan319@gmail.com>
2025-02-13 02:41:55 +0800

    Add GUI

---
+++ src/main/java/bun/ui/Main.java
@@ -0,0 +1,39 @@
+package bun.ui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private Bun bun = new Bun("data/tasks.txt");
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setBun(bun);  // inject the Duke instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Bun getBun() {
+        return bun;
+    }
+
+    public void setBun(Bun bun) {
+        this.bun = bun;
+    }
+}
