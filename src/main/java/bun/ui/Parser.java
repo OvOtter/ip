d15cdb6d9674e3fae1786f32f0d0ac9ef17f3455
OVOtter <shenzihan319@gmail.com>
2025-02-21 20:32:44 +0800

    Improve parsing of deadline event

--- src/main/java/bun/ui/Parser.java
+++ src/main/java/bun/ui/Parser.java
@@ -47,33 +47,41 @@ public class Parser {
         case TODO: {
             String description = fullCommand[1];
             if (description.isEmpty()) {
-                throw new MissingFieldException("description");
+                throw new MissingFieldException("description after the \"todo\" command");
             }
             return new AddCommand(new ToDo(description));
         }
         case DEADLINE: {
             String[] content = fullCommand[1].split(" /by ");
             if (content.length == 0 || content[0].trim().isEmpty()) {
-                throw new MissingFieldException("description");
+                throw new MissingFieldException("description after the \"deadline\" command");
             } else if (content.length == 1) {
-                throw new MissingFieldException("due date");
+                throw new MissingFieldException("due date after the \"/by\" keyword");
             }
             String description = content[0].trim();
             String date = content[1].trim();
             return new AddCommand(new Deadline(description, date));
         }
         case EVENT: {
-            String[] content = fullCommand[1].split(" /from | /to ");
-            if (content.length == 0 || content[0].trim().isEmpty()) {
-                throw new MissingFieldException("description");
-            } else if (content.length == 1 || content[1].trim().isEmpty()) {
-                throw new MissingFieldException("start date");
-            } else if (content.length == 2) {
-                throw new MissingFieldException("end date");
+            String[] description_dates = fullCommand[1].split("/from ");
+
+            if (description_dates.length == 0 || description_dates[0].trim().isEmpty()) {
+                throw new MissingFieldException("description (after the \"event\" command amd before specifying start and end dates");
+            } else if (description_dates.length == 1 || description_dates[1].trim().isEmpty()) {
+                throw new MissingFieldException("start and end dates after the \"/from\" and \"/to\" keywords respectively");
             }
-            String description = content[0].trim();
-            String start = content[1].trim();
-            String end = content[2].trim();
+
+            String[] start_end = description_dates[1].split("/to ");
+
+            if (start_end.length == 0 || start_end[0].trim().isEmpty()) {
+                throw new MissingFieldException("start date after the \"/from\" keyword");
+            } else if (start_end.length == 1 || start_end[1].trim().isEmpty()) {
+                throw new MissingFieldException("end date after the \"/to\" keyword");
+            }
+
+            String description = description_dates[0].trim();
+            String start = start_end[0].trim();
+            String end = start_end[1].trim();
 
             return new AddCommand(new Event(description, start, end));
         }
@@ -86,9 +94,9 @@ public class Parser {
         case DOAFTER: {
             String[] content = fullCommand[1].split(" /after ");
             if (content.length == 0 || content[0].trim().isEmpty()) {
-                throw new MissingFieldException("description");
+                throw new MissingFieldException("description after the \"doafter\" command");
             } else if (content.length == 1) {
-                throw new MissingFieldException("start date");
+                throw new MissingFieldException("start date after the \"/after\" keyword");
             }
             String description = content[0].trim();
             String date = content[1].trim();
